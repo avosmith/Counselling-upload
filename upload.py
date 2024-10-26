493c96eed33fd615ce6d716d9e8f9b4023238b59
avosmith <138115333+avosmith@users.noreply.github.com>
2024-10-26 13:38:11 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -10,7 +10,21 @@ import time
 stream = ['Medical', 'Non-Medical', 'Other']
 classOption = ['9', '10', '11', '12', 'Dropper']
 
-creds = credentials.Certificate("counseling-b30aa-firebase-adminsdk-uamom-90e1db5d4e.json")
+cred = {
+  "type": st.secrets['type'],
+  "project_id": st.secrets["project_id"],
+  "private_key_id": st.secrets["private_key_id"],
+  "private_key": st.secrets["private_key"],
+  "client_email": st.secrets["client_email"],
+  "client_id": st.secrets["client_id"],
+  "auth_uri": st.secrets["auth_uri"],
+  "token_uri": st.secrets["token_uri"],
+  "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+  "client_x509_cert_url": st.secrets["client_x509_cert_ur"],
+  "universe_domain": st.secrets["universe_domain"]
+}
+
+creds = credentials.Certificate(cred)
 try:
     firebase_admin.get_app()
 except ValueError:
